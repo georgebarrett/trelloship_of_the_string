93969a8d259f9f095a331d167c02521f7fcd8a10
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-06-15 23:17:15 +0100

    changing the component files names to be consistent

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { fetchComments, handleSendingNewComment, handleSendingNewLike } from '../../fetchers';
 import Comment from '../comment/Comment';
-import LikeButton from '../LikeButton/LikeButton';
+import LikeButton from '../likeButton/LikeButton';
 import './Post.css';
 
 const Post = ({post}) => {
