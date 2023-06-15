7198bb81f8437b8c83dd576ac27933b371f7ad43
Matt Hammond <mjhammond94@gmail.com>
2023-06-15 13:47:55 +0100

    Update token model

--- api/controllers/tokens.js
+++ api/controllers/tokens.js
@@ -9,11 +9,9 @@ const SessionsController = {
 
     User.findOne({ email: email }).then(async (user) => {
       if (!user) {
-        console.log("auth error: user not found")
-        res.status(401).json({ message: "auth error" });
+        res.status(401).json({ message: "A user with this email address does not exist" });
       } else if (user.password !== password) {
-        console.log("auth error: passwords do not match")
-        res.status(401).json({ message: "auth error" });
+        res.status(401).json({ message: "Email address and password do not match" });
       } else {
         const token = await TokenGenerator.jsonwebtoken(user.id)
         res.status(201).json({ token: token, message: "OK" });
