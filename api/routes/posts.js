11bea6729bde6a543bda3cb77bb9936855129344
awdem <wdemontmollin@gmail.com>
2023-06-15 15:46:14 +0100

    adds route for liking a comment, renames add like to post route and method for clarity

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -6,7 +6,8 @@ const PostsController = require("../controllers/posts");
 router.get("/", PostsController.Index);
 router.post("/", PostsController.Create);
 router.post("/add-comment", PostsController.addCommentToPost);
-router.post("/add-like", PostsController.UpdateLike);
+router.post("/add-like", PostsController.UpdateLikePost);
+router.post("/add-like-comment", PostsController.UpdateLikeComment);
 
 module.exports = router;
 
