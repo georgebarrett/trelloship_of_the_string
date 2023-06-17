0d6a393a78864aee48dfe58e4883be4d0bebaf1f
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-06-17 10:12:10 +0100

    refactored liking a post test

--- frontend/cypress/e2e/liking_a_post.cy.js
+++ frontend/cypress/e2e/liking_a_post.cy.js
@@ -1,18 +1,14 @@
 describe("liking a post", () => {
-  it("increments like by one then returns to 0 after second click", () => {
+  before(() => {
     cy.signup("someone@example.com", "password", "username1", "User Name")
-    .then(() => {
-      cy.visit("/login");
-      cy.get("#email").type("someone@example.com");
-      cy.get("#password").type("password");
-      cy.get("#submit").click()
-      .then(() => {
-        cy.get("#submit-like").click();
-        cy.contains("1");
-  
-        cy.get("#submit-like").click();
-        cy.contains("some message 0");
-      })
-    })
+    cy.login("someone@example.com", "password")
+  })
+
+  it("increments like by one then returns to 0 after second click", async () => {
+    await cy.get("#submit-like").click();
+    cy.contains("some message 1");
+
+    await cy.get("#submit-like").click();
+    cy.contains("some message 0");
   })
 })
--- frontend/cypress/e2e/making_a_post.cy.js
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -3,10 +3,10 @@ describe("Posting a message", () => {
     cy.signup("someone@example.com", "password", "username1", "User Name")
     cy.login("someone@example.com", "password")
   })
-  
-  it("adds to the list of posts with valid input and displays the username", () => {
+
+  it("adds to the list of posts with valid input and displays the username", async () => {
     cy.get("#message").type("Test post")
-    cy.get("#submit").click();
+    await cy.get("#submit").click();
     cy.contains("Test post");
     cy.contains("username1");
   })
