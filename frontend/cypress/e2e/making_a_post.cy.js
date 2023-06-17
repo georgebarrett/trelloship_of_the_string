c618edab0d078d6d0618615ab16cf5e85cd7796d
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-06-17 09:48:16 +0100

    put before into making a post cypress test

--- frontend/cypress/e2e/making_a_post.cy.js
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -1,17 +1,18 @@
 describe("Posting a message", () => {
+  before(() => {
+    cy.signup("someone@example.com", "password", "username1", "User Name")
+  })
     it("adds to the list of posts with valid input and displays the username", () => {
-      cy.signup("someone@example.com", "password", "username1", "User Name")
+      //cy.signup("someone@example.com", "password", "username1", "User Name")
+      cy.visit("/login");
+      cy.get("#email").type("someone@example.com");
+      cy.get("#password").type("password");
+      cy.get("#submit").click()
       .then(() => {
-        cy.visit("/login");
-        cy.get("#email").type("someone@example.com");
-        cy.get("#password").type("password");
-        cy.get("#submit").click()
-        .then(() => {
-          cy.get("#message").type("Test post")
-          cy.get("#submit").click();
-          cy.contains("Test post");
-          cy.contains("username1");
-        })
+        cy.get("#message").type("Test post")
+        cy.get("#submit").click();
+        cy.contains("Test post");
+        cy.contains("username1");
       })
     })
 })
