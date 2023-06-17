ff26b94c582263f178c04c8d7a491d8323b53ea6
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-06-17 10:09:15 +0100

    using async await in comment test

--- frontend/cypress/e2e/commenting_on_a_post.cy.js
+++ frontend/cypress/e2e/commenting_on_a_post.cy.js
@@ -4,22 +4,11 @@ describe("Commenting on a post", () => {
     cy.login("someone@example.com", "password")
   })
 
-  it("Adds a comment to the post with valid input", () => {
-    // cy.signup("someone@example.com", "password", "username1", "User Name")
-    // .then(() => {
-    //   cy.visit("/login");
-    //   cy.get("#email").type("someone@example.com");
-    //   cy.get("#password").type("password");
-    //   cy.get("#submit").click()
-    //   .then(() => {
+  it("Adds a comment to the post with valid input", async () => {
     cy.get('#message').type('my post');
-    cy.get('#submit').click()
-    .then(() => {
-      cy.get('[data-cy="comment-input-field"]').last().type("Making a comment");
-      cy.get('[data-cy="submit-comment"]').last().click();
-    })
-    .then(() => {
-      cy.get('[data-cy="comment"]').last().should('contain.text', 'Making a comment');
-    });
+    await cy.get('#submit').click()
+    cy.get('[data-cy="comment-input-field"]').last().type("Making a comment");
+    await cy.get('[data-cy="submit-comment"]').last().click();
+    cy.get('[data-cy="comment"]').last().should('contain.text', 'Making a comment');
   })
 })
