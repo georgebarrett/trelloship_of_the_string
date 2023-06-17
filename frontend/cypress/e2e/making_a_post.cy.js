06a4d84f97413eaadf203e75bd5461010fe36837
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-06-17 10:03:26 +0100

    using cy.login in the making_a_post test

--- frontend/cypress/e2e/making_a_post.cy.js
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -1,18 +1,19 @@
 describe("Posting a message", () => {
   before(() => {
     cy.signup("someone@example.com", "password", "username1", "User Name")
+    cy.login("someone@example.com", "password")
   })
     it("adds to the list of posts with valid input and displays the username", () => {
       //cy.signup("someone@example.com", "password", "username1", "User Name")
-      cy.visit("/login");
-      cy.get("#email").type("someone@example.com");
-      cy.get("#password").type("password");
-      cy.get("#submit").click()
-      .then(() => {
+      // cy.visit("/login");
+      // cy.get("#email").type("someone@example.com");
+      // cy.get("#password").type("password");
+      // cy.get("#submit").click()
+      //.then(() => {
         cy.get("#message").type("Test post")
         cy.get("#submit").click();
         cy.contains("Test post");
         cy.contains("username1");
-      })
+      //})
     })
 })
